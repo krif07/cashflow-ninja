d9711dfe0a52c2ef32b1d8f91d44745c357be10a
Cristian Fernando Dávila López <krif07@gmail.com>
2024-09-10 19:23:06 -0500

    Starting project

---
+++ src/test/java/com/co/cfd/springboot/cashflow/ninja/cashflow_ninja/CashflowNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.cfd.springboot.cashflow.ninja.cashflow_ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
